2d60f2f134c848488932a46be8bd4bb7443a7df2
prasadadhav <prasada31@yahoo.in>
2025-03-11 17:26:52 +0100

    sample logging and usage of libraries

---
+++ R/00_logging.R
@@ -0,0 +1,7 @@
+message("\033[94m[microbs Info]: This is just some information, everything was fine \033[39m")
+
+message("\033[91m[microbs Report]: This is just a message, everything was fine \033[39m")
+
+message("\033[91m[microbs Warning]: This is just a WARNING, maybe things are fine \033[39m")
+
+message("\033[91m[microbs Error]: This is just a Error, everything was NOT fine \033[39m")
--- R/01_LoadLibs.R
+++ R/01_LoadLibs.R
@@ -1,2 +1,22 @@
-# Here all the required libraries are loaded
-# Add a short comment why/where the library is used
+# #' Load required libraries for the package
+# #'
+# #' This function loads all the necessary libraries for the package.
+# #' Add any new library used in the package to this function.
+# #' 
+# #' @return NULL
+# #' @export
+# load_required_libraries <- function() {
+#     library(readxl)      # For reading Excel files
+#     library(tidyverse)   # For data manipulation and visualization
+#     library(utils)       # For various utility functions
+#     library(janitor)     # For data cleaning
+#     library(plyr)        # For data manipulation (must be loaded before dplyr)
+#     library(dplyr)       # For data manipulation
+#     library(writexl)     # For writing Excel files
+#     library(lubridate)   # For working with dates and times
+#     library(tidyr)       # For tidying data
+#     return(NULL)
+# }
+
+# # Load the libraries when the script is sourced
+# load_required_libraries()
