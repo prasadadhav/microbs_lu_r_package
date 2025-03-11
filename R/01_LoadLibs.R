6ef60afcb8b5593d570001085dd4c49c945db304
prasadadhav <prasada31@yahoo.in>
2025-03-11 10:06:15 +0100

    skeleton r scripts

---
+++ R/01_LoadLibs.R
@@ -0,0 +1,2 @@
+# Here all the required libraries are loaded
+# Add a short comment why/where the library is used
---
+++ R/02_SetPaths.R
@@ -0,0 +1,8 @@
+# Here functions are used to set some crucial paths
+# These paths will contain the data directories for
+# - RAW Data
+# - Checked data
+# - Cleaned data
+# - Calculated data
+# - File creation
+# - Transfer file to file directory
