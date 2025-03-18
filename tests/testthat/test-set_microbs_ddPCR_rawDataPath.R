51bb84dc7323ba7d8996f36df2866606cea4b3f4
prasadadhav <prasada31@yahoo.in>
2025-03-18 11:03:40 +0100

    fix paths in test. updated tests for local path testing.

--- tests/testthat/test-set_microbs_ddPCR_rawDataPath.R
+++ tests/testthat/test-set_microbs_ddPCR_rawDataPath.R
@@ -1,5 +1,5 @@
 test_that("setting the default path for raw ddPCR data", {
-    true_path <- "L:/Units & Programmes/BIOTECH/ENVMICRO/_Common/Projects/SUPERVIR/11-Results/Data Treatment/0_raw_data_ddPCR"
+    true_path <- "L:/Units & Programmes/BIOTECH/ENVMICRO/_Common/Projects/SUPERVIR/11-Results/Data_Treatment/0_raw_data_ddPCR"
 
     test_path <- set_microbs_ddPCR_rawDataPath()
 
@@ -10,11 +10,11 @@ test_that("setting the default path for raw ddPCR data", {
 
 test_that("setting the custom local path for raw ddPCR data", {
     # Set the global variables as they would be set by other functions
-    assign("wd", "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data", envir = .GlobalEnv)
-    assign("path_connector", "Data Treatment", envir = .GlobalEnv)
+    assign("wd", set_microbs_wdirectory("D:/03_Workspace/01_R_Package/microbs_lu_dummy_data"), envir = .GlobalEnv)
+    assign("path_connector", set_microbs_connector_dir(), envir = .GlobalEnv)
 
     test_local_path <- "0_raw_data_ddPCR"
-    true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data Treatment/0_raw_data_ddPCR"
+    true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data_Treatment/0_raw_data_ddPCR"
 
     test_path <- set_microbs_ddPCR_rawDataPath(test_local_path)
 
@@ -26,21 +26,16 @@ test_that("setting the custom local path for raw ddPCR data", {
     rm("path_connector", envir = .GlobalEnv)
 })
 
+test_that("setting the relative path for raw qPCR data", {
+    withr::with_dir("D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data_Treatment",
+    {
+        true_path <- "./0_raw_data_ddPCR"
 
-test_that("setting the relative path for raw ddPCR data", {
-    assign("wd", "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data", envir = .GlobalEnv)
-    assign("path_connector", "Data Treatment", envir = .GlobalEnv)
+        test_local_path <- "0_raw_data_ddPCR"
 
-    true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data Treatment/0_raw_data_ddPCR"
+        test_path <- set_microbs_ddPCR_rawDataPath(test_local_path,  relative=TRUE)
 
-    test_local_path <- "0_raw_data_ddPCR"
-
-    test_path <- set_microbs_ddPCR_rawDataPath(test_local_path)
-
-    # Ensure that the function correctly sets the working directory
-    expect_equal(true_path, test_path)
-
-    # Clean up the global variables
-    rm("wd", envir = .GlobalEnv)
-    rm("path_connector", envir = .GlobalEnv)
+        # Ensure that the function correctly sets the working directory
+        expect_equal(true_path, test_path)
+    })
 })
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,76 +1,76 @@
-test_that("using path builder works well", {
+test_that("using path builder for qPCR raw data", {
   test_global_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data"
-  test_connnector <- "Data Treatment"
+  test_connnector <- "Data_Treatment"
   test_local_path <- "0_raw_data_qPCR"
 
   test_path <- utils_microbs_path_builder(test_global_path, test_connnector, test_local_path)
 
-  true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data Treatment/0_raw_data_qPCR"
+  true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data_Treatment/0_raw_data_qPCR"
 
   # Ensure that the function correctly sets the working directory
   expect_equal(true_path, test_path)
 })
 
-test_that("using path builder works well", {
+test_that("using path builder for ddPCR raw data", {
   test_global_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data"
-  test_connnector <- "Data Treatment"
+  test_connnector <- "Data_Treatment"
   test_local_path <- "0_raw_data_ddPCR"
 
   test_path <- utils_microbs_path_builder(test_global_path, test_connnector, test_local_path)
 
-  true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data Treatment/0_raw_data_ddPCR"
+  true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data_Treatment/0_raw_data_ddPCR"
 
   # Ensure that the function correctly sets the working directory
   expect_equal(true_path, test_path)
 })
 
-test_that("using path builder works well", {
+test_that("using path builder for loaded data", {
   test_global_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data"
-  test_connnector <- "Data Treatment"
+  test_connnector <- "Data_Treatment"
   test_local_path <- "1_loaded_data"
 
   test_path <- utils_microbs_path_builder(test_global_path, test_connnector, test_local_path)
 
-  true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data Treatment/1_loaded_data"
+  true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data_Treatment/1_loaded_data"
 
   # Ensure that the function correctly sets the working directory
   expect_equal(true_path, test_path)
 })
 
-test_that("using path builder works well", {
+test_that("using path builder for calculated data", {
   test_global_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data"
-  test_connnector <- "Data Treatment"
+  test_connnector <- "Data_Treatment"
   test_local_path <- "2_calc_data"
 
   test_path <- utils_microbs_path_builder(test_global_path, test_connnector, test_local_path)
 
-  true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data Treatment/2_calc_data"
+  true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data_Treatment/2_calc_data"
 
   # Ensure that the function correctly sets the working directory
   expect_equal(true_path, test_path)
 })
 
-test_that("using path builder works well", {
+test_that("using path builder for created data", {
   test_global_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data"
-  test_connnector <- "Data Treatment"
+  test_connnector <- "Data_Treatment"
   test_local_path <- "3_created_data"
 
   test_path <- utils_microbs_path_builder(test_global_path, test_connnector, test_local_path)
 
-  true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data Treatment/3_created_data"
+  true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data_Treatment/3_created_data"
 
   # Ensure that the function correctly sets the working directory
   expect_equal(true_path, test_path)
 })
 
-test_that("using path builder works well", {
+test_that("using path builder for data processed for dashboard", {
   test_global_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data"
-  test_connnector <- "Data Treatment"
+  test_connnector <- "Data_Treatment"
   test_local_path <- "4_data_4_dashboard"
 
   test_path <- utils_microbs_path_builder(test_global_path, test_connnector, test_local_path)
 
-  true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data Treatment/4_data_4_dashboard"
+  true_path <- "D:/03_Workspace/01_R_Package/microbs_lu_dummy_data/Data_Treatment/4_data_4_dashboard"
 
   # Ensure that the function correctly sets the working directory
   expect_equal(true_path, test_path)
