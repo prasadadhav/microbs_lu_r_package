e7858805abd53c76300ee4ace700cec95ce86b76
prasadadhav <prasada31@yahoo.in>
2025-03-18 08:46:05 +0100

    deactivating global env variables as we just want the paths, and not set the wd path.

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
 # Decalring global paths to silence the Rmd warnings
 # Initialize globals
-wd <- NULL
-path_connector <- NULL
-ddPCR_raw_path <- NULL
-qPCR_raw_path <- NULL
+# wd <- NULL
+# path_connector <- NULL
+# ddPCR_raw_path <- NULL
+# qPCR_raw_path <- NULL
 
-utils::globalVariables(c("wd", "path_connector", "ddPCR_raw_path", "qPCR_raw_path"))
+# utils::globalVariables(c("wd", "path_connector", "ddPCR_raw_path", "qPCR_raw_path"))
 
