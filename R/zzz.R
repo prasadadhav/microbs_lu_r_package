52370cad87be7e3c714c012d06e582f3812c3dae
Prasad ADHAV <prasada31@yahoo.in>
2025-04-01 16:06:32 +0200

    added global vars for the paths

--- R/zzz.R
+++ R/zzz.R
@@ -10,7 +10,12 @@
 # .microbs_env$flux_path <- NULL
 # .microbs_env$check_data_path <- NULL
 
+# The .onLoad function will load these global variables into the R env.
+# Add any variables here that might be needed to be accessed when running the code
+
 .onLoad <- function(libname, pkgname) {
+
+    # Create the empty paths to store information
     .microbs_env <<- new.env(parent = emptyenv())
     .microbs_env$wd <- NULL
     .microbs_env$path_connector <- NULL
@@ -18,5 +23,10 @@
     .microbs_env$qPCR_raw_path <- NULL 
     .microbs_env$stdCurve_path <- NULL
     .microbs_env$flux_path <- NULL
-    .microbs_env$check_data_path <- NULL
+    .microbs_env$loaded_data_path <- NULL
+    .microbs_env$calc_data_path <- NULL
+    .microbs_env$created_data_path <- NULL
+
+    # Create empty variables to store data
+    .microbs_env$df_flu_raw_ddPCR_data <- NULL
 }
