e29288b30555e6249e64a72876f23d8c69303bc4
prasadadhav <prasada31@yahoo.in>
2025-05-19 17:35:55 +0200

    added data getter

--- R/03_DataGetters.R
+++ R/03_DataGetters.R
@@ -87,6 +87,35 @@ get_microbs_old_raw_qPCR_Data <- function() {
 }
 
 #--------------------------------------------------------------------------------------------------------
+# get Loaded qPCR data
+#--------------------------------------------------------------------------------------------------------
+#' @title get new raw loaded data for qPCR
+#' 
+#' @description If it returns NULL, it means that the new data has not been loaded yet.
+#' Call load_microbs_old_raw_qPCR_Data() to load the data.
+#' Need to set relevant paths.
+#' Preferably use full path.
+#' This function allows you to load the new loaded RAW data located in `1_loaded_data`.
+#' 
+#' file ./microbs.lu/R/03_DataGetters.R
+#' 
+#' @examples
+#' # Example usage
+#' result <- get_microbs_new_raw_qPCR_Data()
+#' 
+#' @examples
+#' result
+#' 
+#' @export 
+get_microbs_new_raw_qPCR_Data <- function() {
+    if (is.null(.microbs_env$df_old_raw_qPCR_data)) {
+        message("[microbs Warning]: Did not yet set the path. Use the load_microbs_raw_qPCR_Data() function to set a path")
+        message("or use the default by using load_microbs_raw_qPCR_Data()")
+    }
+    .microbs_env$df_new_raw_qPCR_data
+}
+
+#--------------------------------------------------------------------------------------------------------
 # get checked ddPCR data
 #--------------------------------------------------------------------------------------------------------
 #' @title get old calc data for ddPCR
