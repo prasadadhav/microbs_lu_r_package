2a18ac2b2634d00027b82e4b5956f941b9828076
Prasad ADHAV <prasada31@yahoo.in>
2025-05-23 16:25:28 +0200

    updatesd imports used

--- R/imports.R
+++ R/imports.R
@@ -1,7 +1,7 @@
-#' @importFrom dplyr select mutate case_when filter distinct arrange add_count if_else
+#' @importFrom dplyr select mutate case_when filter distinct arrange add_count if_else join_by
 #' @importFrom magrittr %>%
-#' @importFrom plyr rbind.fill
+#' @importFrom plyr rbind.fill join
 #' @importFrom readr read_csv
 #' @importFrom writexl write_xlsx
 #' @importFrom base position
-#' @importFrom stringr str_detect regex
+#' @importFrom stringr str_detect regex str_sub
