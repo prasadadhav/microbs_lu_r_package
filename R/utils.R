a46ee72398417b2bcd37a59472422a03cf3b0b36
Prasad ADHAV <prasada31@yahoo.in>
2025-06-10 14:56:05 +0200

    added function to generate yyyy_ww

--- R/utils.R
+++ R/utils.R
@@ -96,3 +96,27 @@ run_microbs_silently <- function(expr, quiet = TRUE) {
 utils_extract_WWTP <- function(sample_column) {
   sub("^([A-Za-z]+)[0-9]+.*$", "\\1", sample_column)
 }
+
+
+#--------------------------------------------------------------------------------------------------------
+#   Generate weeks
+#--------------------------------------------------------------------------------------------------------
+#' @title Generate the sequence of weeks for the given years
+#'
+#' @description This function generates yyyy_ww format for the given year
+#'
+#' @param column A column with strings
+#' @return A column of strings with extract only first alphabets
+#' 
+#' ^([A-Za-z]+) → captures the letters at the beginning
+#' [0-9]+.*$ → skips the rest (digits and any trailing letter like d)
+#'
+generate_weeks <- function(start_year, end_year) {
+    # Créer une séquence de dates pour chaque lundi de chaque semaine
+    dates <- seq.Date(from = as.Date(paste0(start_year, "-01-01")),
+                      to = as.Date(paste0(end_year, "-12-31")),
+                      by = "week")
+    # Extraire l'année et la semaine en format "YYYY_WW"
+    weeks <- format(dates, "%Y_%V")
+    return(unique(weeks))
+}
